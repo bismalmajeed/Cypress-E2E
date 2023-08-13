9695e11502d95345994d9c27f25760249416efc2
Bismal Majeed <77693261+bismalmajeed@users.noreply.github.com>
2023-08-14 01:16:43 +0500

    suite name is changes

--- cypress/e2e/tests/addToCartTest.cy.js
+++ cypress/e2e/tests/addToCartTest.cy.js
@@ -3,7 +3,7 @@ const homePageObj = new homePage()
 import testData from '../../fixtures/testData.json'
 
 
-describe(' test automation', () => {
+describe(' test automation for cypress', () => {
     before(() => {
         cy.login(testData.login.username, testData.login.password)
     })
--- cypress/e2e/tests/registerTest.cy.js
+++ cypress/e2e/tests/registerTest.cy.js
@@ -2,7 +2,7 @@ import { registerPage } from "../../pages/registerPage"
 const registerObj = new registerPage()
 import registerData from '../../fixtures/registerData.json'
 
-describe(' test automation', () => {
+describe(' test automation cypress', () => {
 
 
 
